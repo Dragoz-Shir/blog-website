d4a6b277a13f5331e3f8c301c88d4dc25688cd96
Dragoz Morgado <dragozcm@gmail.com>
2020-09-09 12:36:17 -0500

    agregado la persistencia de datos, falta remover el metodo find por findOne en el metodo post

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -19,14 +20,42 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const posts = [];
+mongoose.connect("mongodb://localhost/daylyJournal", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const dailySchema = new mongoose.Schema({
+  titulo: {
+    required: true,
+    type: String,
+  },
+  contenido: {
+    required: true,
+    type: String,
+  },
+});
+const Daily = mongoose.model("Daily", dailySchema);
+/*const daylyPost = Daily.create(
+  { titulo: "Prueba", contenido: "algo así a lo loco alv" },
+  function (e, bien) {
+    if (!e) {
+      console.log("todo bien: " + bien);
+    }
+  }
+);*/
+//const posts = [];
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
 
 app.get("/", function (req, res) {
-  res.render("home", { homeInfo: homeStartingContent, posts: posts });
+  Daily.find({}, function (e, postFound) {
+    if (!e) {
+      res.render("home", { homeInfo: homeStartingContent, posts: postFound });
+      //console.log(postFound);
+    }
+  });
 });
 app.get("/about", function (req, res) {
   res.render("about", { aboutInfo: aboutContent });
@@ -39,21 +68,43 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  let post = {
-    title: req.body.titulo,
-    contenido: req.body.contenido,
-  };
+  const title = req.body.titulo;
+  const content = req.body.contenido;
 
-  posts.push(post);
+  const post = new Daily({
+    titulo: title,
+    contenido: content,
+  });
+  post.save();
   res.redirect("/");
+
+  //  posts.push(post);
 });
 
 app.get("/posts/:topics", function (req, res) {
-  let buscar = _.lowerCase(req.params.topics);
+  /*let buscar = _.lowerCase(req.params.topics);
 
-  posts.forEach(function (post) {
+  posts.forEach(function (post) {x
     if (_.lowerCase(post.title) === buscar) {
       res.render("post", { title: post.title, info: post.contenido });
+    }
+  });*/
+  const id = req.params.topics;
+  Daily.find({ _id: id }, function (e, postByID) {
+    if (!e && postByID != null) {
+      console.log(
+        "lo encontre este es el post: " +
+          postByID +
+          "Titulo" +
+          postByID[0].titulo
+      );
+
+      res.render("post", {
+        title: postByID[0].titulo,
+        info: postByID[0].contenido,
+      });
+      // console.log("Titulo: " + postFound.titulo);
+      // console.log("contenido: " + postFound.contenido);
     }
   });
 });
